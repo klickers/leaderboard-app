510241c493526b78cf1611edafa8a5d5b27a9035
Catherine Chen <bluebirid228@gmail.com>
2025-09-30 14:42:31 +0300

    add sample leaderboard (line graph)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 import db from "@astrojs/db"
 import vercel from "@astrojs/vercel"
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
-	integrations: [db()],
+	integrations: [db(), icon()],
 	adapter: vercel(),
 	output: "server",
 })
